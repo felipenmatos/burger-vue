3320a595782d5b7e47d8355ffebb29a4bdbef029
felipenmatos <notfelipe2@hotmail.com>
2022-09-24 00:55:28 -0300

    feat: image implementation in header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
-<script setup lang="ts"></script>
-
 <template>
-  <Header />
-  <Footer />
+  <div>
+    <Header :logo="logo_src" :alt="app_name" />
+    <Footer />
+  </div>
 </template>
 
-<script>
+<script lang="ts">
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 export default {
   components: {
     Header,
     Footer,
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burger",
+    };
   },
 };
 </script>
