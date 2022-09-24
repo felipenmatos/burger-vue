4d6e0c3a9624fccd48dd6622d50069e8ecdc3beb
felipenmatos <notfelipe2@hotmail.com>
2022-09-24 00:35:33 -0300

    feat: navbar and footer writing adjustment

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,16 @@
 
 <template>
   <Header />
+  <Footer />
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 export default {
   components: {
     Header,
+    Footer,
   },
 };
 </script>
