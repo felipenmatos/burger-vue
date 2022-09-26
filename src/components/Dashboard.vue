6f7668a26cc0da6fd474d3d84d6148ffb8e0fbc9
felipenmatos <notfelipe2@hotmail.com>
2022-09-26 17:59:24 -0300

    feat: searching for order status and listing

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -26,6 +26,14 @@
         <div>
           <select name="status" class="status">
             <option value="">Selecione:</option>
+            <option
+              v-for="s in status"
+              :key="s.id"
+              value="s.tipo"
+              :selected="burger.status == s.tipo"
+            >
+              {{ s.tipo }}
+            </option>
           </select>
           <button class="delete-btn">Cancelar</button>
         </div>
@@ -55,6 +63,15 @@ export default {
       console.log(this.burgers);
 
       //resgatar os status
+
+      this.getStatus();
+    },
+    async getStatus() {
+      const req = await fetch("http://localhost:3000/status");
+
+      const data = await req.json();
+
+      this.status = data;
     },
   },
   mounted() {
